919dd2737e19a7ee12c74cf391c7be6c662ef546
Dray <719328408@qq.com>
2021-05-01 11:37:00 +0800

    [modify]fix parse method Exception

--- application/src/main/java/com/dr/code/diff/vercontrol/VersionControl.java
+++ application/src/main/java/com/dr/code/diff/vercontrol/VersionControl.java
@@ -1,17 +1,21 @@
 package com.dr.code.diff.vercontrol;
 
+import com.alibaba.fastjson.JSON;
 import com.dr.code.diff.dto.ClassInfoResult;
 import com.dr.code.diff.dto.DiffEntryDto;
 import com.dr.code.diff.dto.MethodInfoResult;
 import com.dr.code.diff.dto.VersionControlDto;
 import com.dr.code.diff.enums.CodeManageTypeEnum;
 import com.dr.code.diff.util.MethodParserUtils;
+import com.dr.common.log.LoggerUtil;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
@@ -29,6 +33,7 @@ import java.util.stream.Collectors;
  * Copyright: Copyright (c) 2021
  */
 @Data
+@Slf4j
 public abstract class VersionControl {
 
     protected VersionControlDto versionControlDto;
@@ -92,25 +97,73 @@ public abstract class VersionControl {
         if(CollectionUtils.isEmpty(versionControlDto.getDiffClasses())){
             return null;
         }
-        List<CompletableFuture<ClassInfoResult>> priceFuture = versionControlDto.getDiffClasses().stream()
-                .map(item -> getClassMethods(getLocalOldPath(item.getNewPath()), getLocalNewPath(item.getNewPath()), item))
+        LoggerUtil.info(log,"需要对比的差异类数",versionControlDto.getDiffClasses().size());
+//        List<CompletableFuture<ClassInfoResult>> priceFuture = versionControlDto.getDiffClasses().stream()
+//                .map(item -> getClassMethods(getLocalOldPath(item.getNewPath()), getLocalNewPath(item.getNewPath()), item))
+//                .collect(Collectors.toList());
+
+//        CompletableFuture.allOf(priceFuture.stream().toArray(CompletableFuture[]::new)).join();
+        return versionControlDto.getDiffClasses().stream()
+                .map(item -> getSycnClassMethods(getLocalOldPath(item.getNewPath()), getLocalNewPath(item.getNewPath()), item))
                 .collect(Collectors.toList());
-        return priceFuture.stream().map(CompletableFuture::join).filter(Objects::nonNull).collect(Collectors.toList());
+//        return priceFuture.stream().map(CompletableFuture::join).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
 
 
-    /**
-     * 获取类的增量方法
-     *
-     * @param oldClassFile 旧类的本地地址
-     * @param mewClassFile 新类的本地地址
-     * @param diffEntry    差异类
-     * @return
-     */
-    private CompletableFuture<ClassInfoResult> getClassMethods(String oldClassFile, String mewClassFile, DiffEntryDto diffEntry) {
+//    /**
+//     * 获取类的增量方法
+//     *
+//     * @param oldClassFile 旧类的本地地址
+//     * @param mewClassFile 新类的本地地址
+//     * @param diffEntry    差异类
+//     * @return
+//     */
+//    private CompletableFuture<ClassInfoResult> getClassMethods(String oldClassFile, String mewClassFile, DiffEntryDto diffEntry) {
+//        //多线程获取差异方法，此处只要考虑增量代码太多的情况下，每个类都需要遍历所有方法，采用多线程方式加快速度
+//        return CompletableFuture.supplyAsync(() -> {
+//            String className = diffEntry.getNewPath().split("\\.")[0].split("src/main/java/")[1];
+//            String moduleName = diffEntry.getNewPath().split("/")[0];
+//            //新增类直接标记，不用计算方法
+//            if (DiffEntry.ChangeType.ADD.equals(diffEntry.getChangeType())) {
+//                return ClassInfoResult.builder()
+//                        .classFile(className)
+//                        .type(DiffEntry.ChangeType.ADD.name())
+//                        .moduleName(moduleName)
+//                        .build();
+//            }
+//            List<MethodInfoResult> diffMethods;
+//            //获取新类的所有方法
+//            List<MethodInfoResult> newMethodInfoResults = MethodParserUtils.parseMethods(mewClassFile);
+//            //如果新类为空，没必要比较
+//            if (CollectionUtils.isEmpty(newMethodInfoResults)) {
+//                return null;
+//            }
+//            //获取旧类的所有方法
+//            List<MethodInfoResult> oldMethodInfoResults = MethodParserUtils.parseMethods(oldClassFile);
+//            //如果旧类为空，新类的方法所有为增量
+//            if (CollectionUtils.isEmpty(oldMethodInfoResults)) {
+//                diffMethods = newMethodInfoResults;
+//            } else {   //否则，计算增量方法
+//                List<String> md5s = oldMethodInfoResults.stream().map(MethodInfoResult::getMd5).collect(Collectors.toList());
+//                diffMethods = newMethodInfoResults.stream().filter(m -> !md5s.contains(m.getMd5())).collect(Collectors.toList());
+//            }
+//            LoggerUtil.info(log, "差异方法",JSON.toJSONString(diffMethods));
+//            //没有增量方法，过滤掉
+//            if (CollectionUtils.isEmpty(diffMethods)) {
+//                return null;
+//            }
+//            return ClassInfoResult.builder()
+//                    .classFile(className)
+//                    .methodInfos(diffMethods)
+//                    .type(DiffEntry.ChangeType.MODIFY.name())
+//                    .moduleName(moduleName)
+//                    .build();
+//        }, executor);
+//    }
+
+    private ClassInfoResult getSycnClassMethods(String oldClassFile, String mewClassFile, DiffEntryDto diffEntry) {
         //多线程获取差异方法，此处只要考虑增量代码太多的情况下，每个类都需要遍历所有方法，采用多线程方式加快速度
-        return CompletableFuture.supplyAsync(() -> {
             String className = diffEntry.getNewPath().split("\\.")[0].split("src/main/java/")[1];
             String moduleName = diffEntry.getNewPath().split("/")[0];
             //新增类直接标记，不用计算方法
@@ -147,7 +200,6 @@ public abstract class VersionControl {
                     .type(DiffEntry.ChangeType.MODIFY.name())
                     .moduleName(moduleName)
                     .build();
-        }, executor);
     }
 
 
