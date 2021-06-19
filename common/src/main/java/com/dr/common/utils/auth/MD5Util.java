46adc7bc6d3420c61f635fd682c42d8b1d640d31
rayduan <719328408@qq.com>
2021-06-19 20:29:03 +0800

    [modify]修复svn路径会覆盖的问题

--- common/src/main/java/com/dr/common/utils/auth/MD5Util.java
+++ common/src/main/java/com/dr/common/utils/auth/MD5Util.java
@@ -10,15 +10,17 @@ public class MD5Util {
 
     private static String byteArrayToHexString(byte b[]) {
         StringBuffer resultSb = new StringBuffer();
-        for (int i = 0; i < b.length; i++)
+        for (int i = 0; i < b.length; i++) {
             resultSb.append(byteToHexString(b[i]));
+        }
         return resultSb.toString();
     }
 
     private static String byteToHexString(byte b) {
         int n = b;
-        if (n < 0)
+        if (n < 0) {
             n += 256;
+        }
         int d1 = n / 16;
         int d2 = n % 16;
         return hexDigits[d1] + hexDigits[d2];
@@ -29,12 +31,13 @@ public class MD5Util {
         try {
             resultString = new String(origin);
             MessageDigest md = MessageDigest.getInstance("MD5");
-            if (charsetname == null || "".equals(charsetname))
+            if (charsetname == null || "".equals(charsetname)) {
                 resultString = byteArrayToHexString(md.digest(resultString
                         .getBytes()));
-            else
+            } else {
                 resultString = byteArrayToHexString(md.digest(resultString
                         .getBytes(charsetname)));
+            }
         } catch (Exception exception) {
             exception.printStackTrace();
         }
