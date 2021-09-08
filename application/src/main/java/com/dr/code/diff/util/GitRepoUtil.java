5eca0308727bded21576c8756e9f93b53fd03109
rayduan <719328408@qq.com>
2021-09-08 11:41:37 +0800

    ssh bug修复

--- application/src/main/java/com/dr/code/diff/util/GitRepoUtil.java
+++ application/src/main/java/com/dr/code/diff/util/GitRepoUtil.java
@@ -1,19 +1,17 @@
 package com.dr.code.diff.util;
 
+import cn.hutool.core.io.FileUtil;
 import com.dr.code.diff.enums.GitUrlTypeEnum;
 import com.dr.common.errorcode.BizCode;
 import com.dr.common.exception.BizException;
 import com.dr.common.log.LoggerUtil;
-import com.dr.common.utils.file.FileUtil;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import lombok.extern.slf4j.Slf4j;
-import org.antlr.runtime.tree.TreeParser;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.TransportConfigCallback;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectReader;
@@ -24,8 +22,8 @@ import org.eclipse.jgit.transport.*;
 import org.eclipse.jgit.treewalk.AbstractTreeIterator;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.eclipse.jgit.util.FS;
+import org.eclipse.jgit.util.FileUtils;
 import org.springframework.util.StringUtils;
-import org.springframework.web.servlet.tags.UrlTag;
 
 import java.io.File;
 import java.io.IOException;
@@ -57,7 +55,7 @@ public class GitRepoUtil {
     }
 
 
-    public static Git instanceSshGit(String gitUrl, String codePath, String commitId, String privateKey) throws GitAPIException {
+    public static Git instanceSshGit(String gitUrl, String codePath, String commitId, Git git, String privateKey) throws GitAPIException {
         SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
             @Override
             protected JSch createDefaultJSch(FS fs) throws JSchException {
@@ -71,6 +69,13 @@ public class GitRepoUtil {
                 session.setConfig("StrictHostKeyChecking", "no");
             }
         };
+        if (null != git) {
+            git.pull().setTransportConfigCallback(transport -> {
+                SshTransport sshTransport = (SshTransport) transport;
+                sshTransport.setSshSessionFactory(sshSessionFactory);
+            }).call();
+            return git;
+        }
         return Git.cloneRepository()
                 .setURI(gitUrl)
                 .setTransportConfigCallback(transport -> {
@@ -129,17 +134,25 @@ public class GitRepoUtil {
     public static Git sshCloneRepository(String gitUrl, String codePath, String commitId, String privateKey) {
         Git git = null;
         try {
-            //ssh无法读取本地代码仓，先删除再clone
-            File repoGitDir = new File(codePath + "/.git");
-            if (repoGitDir.exists()) {
-                FileUtil.removeDir(new File(codePath));
+            if (!checkGitWorkSpace(gitUrl, codePath)) {
+                LoggerUtil.info(log, "本地代码不存在，clone", gitUrl, codePath);
+                //为了区分ssh路径和http/s这里ssh多加了一层目录
+                git = instanceSshGit(gitUrl, codePath, commitId, null, privateKey);
+                // 下载指定branch,ssh好像不支持commitId
+                git.checkout().setName(commitId).call();
+            } else {
+                LoggerUtil.info(log, "本地代码存在,直接使用", gitUrl, codePath);
+                git = Git.open(new File(codePath));
+                git.getRepository().getFullBranch();
+                //判断是分支还是commitId，分支做更新，commitId无法改变用原有的
+                if (git.getRepository().exactRef(Constants.HEAD).isSymbolic()) {
+                    //更新代码
+                    instanceSshGit(gitUrl, codePath, commitId, git, privateKey);
+                }
             }
-            LoggerUtil.info(log, "本地代码不存在，clone", gitUrl, codePath);
-            //为了区分ssh路径和http/s这里ssh多加了一层目录
-            git = instanceSshGit(gitUrl, codePath, commitId, privateKey);
-            // 下载指定branch,ssh好像不支持commitId
-            git.checkout().setName(commitId).call();
-        } catch (GitAPIException e) {
+
+        } catch (GitAPIException |
+                IOException e) {
             if (e instanceof GitAPIException) {
                 throw new BizException(BizCode.GIT_AUTH_FAILED.getCode(), e.getMessage());
             }
@@ -200,7 +213,8 @@ public class GitRepoUtil {
             isExist = Boolean.TRUE;
         } else {
             LoggerUtil.info(log, "本地存在其他仓的代码，先删除");
-            FileUtil.removeDir(new File(codePath));
+            git.close();
+            FileUtils.delete(new File(codePath));
         }
         return isExist;
     }
@@ -246,6 +260,7 @@ public class GitRepoUtil {
             localDir.append("/");
             localDir.append(version);
         }
+        localDir.append("/");
         return localDir.toString();
     }
 
